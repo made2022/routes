74de5833ae3019bea8f7661f12036f1890c177ee
made2022 <98543115+made2022@users.noreply.github.com>
2022-04-09 22:43:02 +0200

    commands

---
+++ commands.js
@@ -0,0 +1,26 @@
+ver ramas: git branch
+iniciar git: git init
+iniciar git flow: git flow init
+clonar repositorio: git clone
+pasar de rama: git checkout
+agregar cambios: git add .
+crear rama con git flow: git flow feature start
+hacer commit: git commit -m "nombre del commit"
+subir cambios a github: git push
+instalar dependencias: npm install
+guardar cambios locales: git stash
+traer cambios del stash: git stash pop
+
+Power Shell 
+navegar a la carpeta de react= cd /users/strider/Desktop/React
+
+para crear todo el proyecto react= npx create-react-app hook-app 
+
+
+el .map recorre los objetos que hay dentro de un array
+.length es la cantidad de objetos que hay dentro de un array
+key se coloca en la primera etiqueta dentro de un .map
+
+para identificar los errores en visual studio console.log("AppRouter") se le coloca cualquier nombre 
+
+
